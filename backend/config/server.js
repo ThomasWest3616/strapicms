9b4ee959dfdf26d88ae8f9329fd9a08e7abeee72
Thomas WEst <thom8750@stud.kea.dk>
2021-11-30 12:23:44 +0100

    first commit

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bc0cb252674cd74de44e9a9ea112d447'),
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b66fca06-2158-4b32-8670-26633ca0c3c8'
+};
